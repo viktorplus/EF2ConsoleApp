f10959742eff6af2864315cfb149f1beec1cf59a
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-12-09 01:39:36 +0100

    Добавьте файлы проекта.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Models;
+using DBContext;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ DBContext/DBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+namespace DBContext
+{
+    public class CommandContext: DbContext
+    {
+        public DbSet<Command> Commands { get; set; }
+        public CommandContext(DbContextOptions<CommandContext> options) : base(options)
+        {
+        }
+    }
+}
